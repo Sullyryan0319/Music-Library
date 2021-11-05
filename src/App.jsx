49bd810b00473004e632f674e7a14690ec01643a
Ryan Sullivan <ryan.sullivan0319@gmail.com>
2021-11-04 20:40:16 -0600

    Create 3 starting react component files within component folder i.e. Music Table, Filter Function, Search Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
-import './App.css';
+import MusicTable from "./components/MusicTable";
 import Axios from 'axios';
 
+import './App.css';
+import axios from 'axios';
 
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
 
+        }
+    }
+}
 
-
+async componentDidMount() {
+    await axios.get('http://www.devcodecampmusiclibrary.com/')
+}
