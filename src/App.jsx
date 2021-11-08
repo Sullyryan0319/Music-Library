b1027cc836cac6e0d9bcdaae3f8e3d4506aee689
Ryan Sullivan <ryan.sullivan0319@gmail.com>
2021-11-07 17:04:39 -0700

    Begin styling and placement of components

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import MusicTable from "./components/MusicTable";
-import axios from 'axios';
-import SearchBar from './components/SearchBar';
+import axios from "axios";
+import SearchBar from "./components/SearchBar";
 
-import './App.css';
-
+import "./App.css";
 
 class App extends Component {
-    constructor() {
-        super();
-        this.handleChange = this.handleChange.bind(this);
-        
-    
-        
-        this.state = {
-            music: [],
-            ui: ''
-            
-        };
-    }
-    
-    handleChange(e) {
-        this.setState({ui: e.target.value});
-    }
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
 
-    async componentDidMount() {
-        await axios.get("http://www.devcodecampmusiclibrary.com/api/music")
-        .then((res) => {
-            console.log(res)
-            const music = res.data;
-            this.setState({music});
-        });
-    }
+    this.state = {
+      music: [],
+      ui: "",
+    };
+  }
 
-        componentDidUpdate() {}
+  handleChange(e) {
+    this.setState({ ui: e.target.value });
+  }
 
-render() {
-    return(
-        <div>
-            <SearchBar ui={this.state.ui} handleChange={this.handleChange}/>
-            <MusicTable music={this.state.music} ui={this.state.ui} />
+  async componentDidMount() {
+    await axios
+      .get("http://www.devcodecampmusiclibrary.com/api/music")
+      .then((res) => {
+        console.log(res);
+        const music = res.data;
+        this.setState({ music });
+      });
+  }
+
+  componentDidUpdate() {}
+
+  render() {
+    return (
+
+      <div>
+        <div className="search-format grid">
+          <SearchBar ui={this.state.ui} handleChange={this.handleChange} />
         </div>
-        
-    )
+        <div className="table-format grid">
+          <MusicTable music={this.state.music} ui={this.state.ui} />
+        </div>
+      </div>
+    );
+  }
 }
 
-}
-
-export default App; 
+export default App;
